8975715461c50f81dd9dac406822bf9b8ff91ccd
ICX\henadzi.miasnikou <job.myasnikov.gennady@gmail.com>
2018-03-23 11:44:34 +0300

    Post mapping extension added

---
+++ VooDiDb/VooDiDb.Services.Core/MappingExtensions/PostMappingExtension.cs
@@ -0,0 +1,25 @@
+﻿using VooDiDb.Domain.Core;
+
+namespace VooDiDb.Services.Core.MappingExtensions {
+    public static class PostMappingExtension {
+        public static PostDTO MapToPostDTO(this Post entity) {
+            return new PostDTO {
+                Id = entity.Id,
+                Name = entity.Name,
+                RowVersion = entity.RowVersion,
+                IsActive = !entity.IsDeleted,
+                SortOrder = entity.SortOrder
+            };
+        }
+
+        public static Post MapToPost(this PostDTO dto) {
+            return new Post {
+                Id = dto.Id,
+                Name = dto.Name,
+                IsDeleted = !dto.IsActive,
+                RowVersion = dto.RowVersion,
+                SortOrder = dto.SortOrder
+            };
+        }
+    }
+}
