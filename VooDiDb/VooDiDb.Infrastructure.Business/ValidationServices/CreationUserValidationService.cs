744a52dbf5233716cdd5e330809f3ea0f2e475ea
ICX\henadzi.miasnikou <job.myasnikov.gennady@gmail.com>
2018-03-23 11:38:03 +0300

    User business logic improved

--- VooDiDb/VooDiDb.Infrastructure.Business/ValidationServices/CreationUserValidationService.cs
+++ VooDiDb/VooDiDb.Infrastructure.Business/ValidationServices/CreationUserValidationService.cs
@@ -6,26 +6,21 @@ using VooDiDb.Domain.Interfaces;
 using VooDiDb.Services.Core;
 using VooDiDb.Services.Core.Validation;
 
-namespace VooDiDb.Infrastructure.Business.ValidationServices
-{
-    public class CreationUserValidationService : IValidationService<UserDTO>
-    {
+namespace VooDiDb.Infrastructure.Business.ValidationServices {
+    public class CreationUserValidationService : IValidationService<UserRegistrationDTO> {
         private readonly IRepository<User> repository;
 
-        public CreationUserValidationService(IRepository<User> repository)
-        {
+        public CreationUserValidationService(IRepository<User> repository) {
             this.repository = repository;
         }
 
-        public IEnumerable<ValidationResult> Validate(UserDTO item)
-        {
-            if (repository.Any(x => x.Login == item.Login))
+        public IEnumerable<ValidationResult> Validate(UserRegistrationDTO item) {
+            if(this.repository.Any(x => x.Login == item.Login))
                 yield return new ValidationResult($"Another user has login '{item.Login}'");
         }
 
-        public object GetService(Type serviceType)
-        {
-            return serviceType == GetType() ? this : null;
+        public object GetService(Type serviceType) {
+            return serviceType == this.GetType() ? this : null;
         }
     }
 }
---
+++ VooDiDb/VooDiDb.Infrastructure.Business/ValidationServices/EditionUserValidationService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using VooDiDb.Domain.Core;
+using VooDiDb.Domain.Interfaces;
+using VooDiDb.Services.Core;
+using VooDiDb.Services.Core.Validation;
+
+namespace VooDiDb.Infrastructure.Business.ValidationServices {
+    public class EditionUserValidationService : IValidationService<UserDTO> {
+        private readonly IRepository<User> repository;
+
+        public EditionUserValidationService(IRepository<User> repository) {
+            this.repository = repository;
+        }
+
+        public IEnumerable<ValidationResult> Validate(UserDTO item) {
+            if(item == null) {
+                yield return new ValidationResult("User can not be null.");
+                yield break;
+            }
+
+            var entry = this.repository.FindById(item.Id);
+            if(entry == null) {
+                yield return new ValidationResult("User not found.");
+                yield break;
+            }
+
+            if(!entry.RowVersion.SequenceEqual(item.RowVersion))
+                yield return new ValidationResult("Version of user is not compatible", new[] { nameof(item.RowVersion) });
+        }
+
+        public object GetService(Type serviceType) {
+            return serviceType == this.GetType() ? this : null;
+        }
+    }
+}
